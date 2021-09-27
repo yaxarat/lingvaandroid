2e5bec058dc79a19ca7bbd0a0ba964fa3e558c4e
Yashar Atajan <yaxarat@users.noreply.github.com>
2021-09-26 20:03:05 -0400

    Added translate button for more clear translation intent

--- app/src/main/java/dev/atajan/lingva_android/ui/screens/TranslateScreen.kt
+++ app/src/main/java/dev/atajan/lingva_android/ui/screens/TranslateScreen.kt
@@ -2,23 +2,19 @@ package dev.atajan.lingva_android.ui.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Translate
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -35,7 +31,11 @@ fun TranslatenScreen(
     val softwareKeyboardController = LocalSoftwareKeyboardController.current
     val textToTranslateMutableState = viewModel.textToTranslate
 
-    Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.background)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
         LanguageSelectionBar(
             modifier = Modifier.padding(all = 16.dp),
             supportedLanguages = viewModel.supportedLanguages,
@@ -45,23 +45,47 @@ fun TranslatenScreen(
             toggleTheme = toggleTheme
         )
 
-        OutlinedTextField(
-            value = textToTranslateMutableState.value,
-            onValueChange = { textToTranslateMutableState.value = it },
-            label = { Text("Source text") },
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.35f)
-                .padding(all = 16.dp),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    softwareKeyboardController?.hide()
-                    viewModel.translate()
+                .padding(all = 16.dp)
+        ) {
+            OutlinedTextField(
+                value = textToTranslateMutableState.value,
+                onValueChange = { textToTranslateMutableState.value = it },
+                label = { Text("Source text") },
+                modifier = Modifier.fillMaxSize(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        softwareKeyboardController?.hide()
+                        viewModel.translate()
+                    }
+                ),
+                textStyle = TextStyle(color = MaterialTheme.colors.onSurface)
+            )
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.End
+            ) {
+                Spacer(modifier = Modifier.fillMaxSize(0.80f))
+                IconButton(
+                    onClick = {
+                        softwareKeyboardController?.hide()
+                        viewModel.translate()
+                    },
+                    modifier = Modifier.fillMaxHeight().padding(bottom = 16.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Translate,
+                        contentDescription = "Translate",
+                        tint = if (isDarkTheme.value) Color.White else Color.Black,
+                    )
                 }
-            ),
-            textStyle = TextStyle(color = MaterialTheme.colors.onSurface)
-        )
+            }
+        }
 
         Divider(modifier = Modifier.padding(horizontal = 16.dp))
 
