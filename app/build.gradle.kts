b3f843b4b645de299beb000df2b6d04102d17f51
Yashar Atajan <yaxarat@users.noreply.github.com>
2021-09-26 21:04:30 -0400

    Shrink release size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
