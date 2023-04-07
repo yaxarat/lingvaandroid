90fd06503c7774895511257e04558d4ecdeb6024
Yashar <ya.atajan@gmail.com>
2023-04-06 21:15:07 -0400

    added feature to display pronunciation

--- app/src/main/java/dev/atajan/lingva_android/translatefeature/redux/TranslateScreenState.kt
+++ app/src/main/java/dev/atajan/lingva_android/translatefeature/redux/TranslateScreenState.kt
@@ -5,9 +5,11 @@ import dev.atajan.lingva_android.common.domain.models.language.Language
 data class TranslateScreenState(
     val supportedLanguages: List<Language> = emptyList(),
     val translatedText: String = "",
+    val translatedTextPronunciation: String = "",
     val sourceLanguage: Language = Language("auto", "Detect"),
     val targetLanguage: Language = Language("es", "Spanish"),
     val errorDialogState: Boolean = false,
+    val displayPronunciation: Boolean = false,
     val defaultSourceLanguage: String = "",
     val defaultTargetLanguage: String = "",
 )
