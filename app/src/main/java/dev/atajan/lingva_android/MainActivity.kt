baba4829e606b60face453c2738955cf4fdb9623
Yashar <ya.atajan@gmail.com>
2023-04-23 15:13:09 -0400

    Crash fix

--- app/src/main/java/dev/atajan/lingva_android/MainActivity.kt
+++ app/src/main/java/dev/atajan/lingva_android/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import dev.atajan.lingva_android.translatefeature.screens.TranslateScreenViewModel
-import dev.atajan.lingva_android.translatefeature.screens.TranslationScreen
 import dev.atajan.lingva_android.common.ui.theme.LingvaAndroidTheme
 import dev.atajan.lingva_android.common.ui.theme.ThemingOptions
 import dev.atajan.lingva_android.common.ui.theme.canUseDynamicColor
 import dev.atajan.lingva_android.common.ui.theme.isSystemInNightMode
 import dev.atajan.lingva_android.common.ui.theme.selectedThemeFlow
+import dev.atajan.lingva_android.translatefeature.screens.TranslateScreenViewModel
+import dev.atajan.lingva_android.translatefeature.screens.TranslationScreen
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -24,20 +24,20 @@ import kotlinx.coroutines.flow.onEach
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private var theme: MutableState<ThemingOptions> = mutableStateOf(
-        if (canUseDynamicColor) {
-            ThemingOptions.YOU
-        } else if (isSystemInNightMode(this)) {
-            ThemingOptions.DARK
-        } else {
-            ThemingOptions.LIGHT
-        }
-    )
-
     private val viewModel: TranslateScreenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val theme: MutableState<ThemingOptions> = mutableStateOf(
+            if (canUseDynamicColor) {
+                ThemingOptions.YOU
+            } else if (isSystemInNightMode(this)) {
+                ThemingOptions.DARK
+            } else {
+                ThemingOptions.LIGHT
+            }
+        )
 
         selectedThemeFlow(applicationContext)
             .onEach {
